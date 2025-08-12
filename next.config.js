6ab52853c7a9f4d243413500afe19f60159c9e63
kohli-pushpanshu <kohli-pushpanshu@users.noreply.github.com>
2025-08-12 18:00:07 +0530

    Changes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  /* config options here */
-};
-module.exports = {
-  images: {
-    domains: ['images.unsplash.com', 'cdn.example.com'], // Add any external domains you're using
-  },
+  // your config options
 };
 
-export default nextConfig;
+module.exports = nextConfig;
